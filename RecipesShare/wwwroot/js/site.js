e68bd1659cd761d7b368c73e6a0dec69db2f8266
ViktoR <Kachamakov@abv.bg>
2024-03-27 17:02:21 +0200

    Show/Hide Recipe Description JS added

--- RecipesShare/wwwroot/js/site.js
+++ RecipesShare/wwwroot/js/site.js
@@ -1,4 +1,28 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿//const toggleButtons = document.querySelectorAll('.toggle-description-btn');
+//const descriptionElements = document.querySelectorAll('.recipe-description');
 
-// Write your JavaScript code.
+//// Function to toggle description visibility
+//function toggleDescription(event) {
+//    const cardBody = event.target.closest('.card-body');
+//    const descriptionElement = cardBody.querySelector('.recipe-description');
+//    descriptionElement.classList.toggle('d-none');
+//}
+
+//// Add click event listener to each toggle button
+//toggleButtons.forEach(button => {
+//    button.addEventListener('click', toggleDescription);
+//});
+// Get all toggle buttons and description elements
+const toggleButtons = document.querySelectorAll('.toggle-description-btn');
+
+// Function to toggle description visibility
+function toggleDescription(event) {
+    const cardBody = event.target.closest('.card-body');
+    const descriptionElement = cardBody.querySelector('.recipe-description');
+    descriptionElement.classList.toggle('d-none');
+}
+
+// Add click event listener to each toggle button
+toggleButtons.forEach(button => {
+    button.addEventListener('click', toggleDescription);
+});
